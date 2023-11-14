7c55d30ca9baca4e150ec4e6074e041138a4249b
Samuel Marino <samuel.marino@ip-192-168-4-57.eu-west-1.compute.internal>
2023-11-14 16:09:09 +0000

    Major revisions

--- mac-suppress-energy-pop-up-v2.sh
+++ mac-suppress-energy-pop-up-v2.sh
@@ -1,44 +1,66 @@
 #!/bin/zsh
 
+max_retries=15
+attempt=0
 
-# Initialize currUser as an empty string
-currUser=""
+while (( attempt < max_retries )); do
+    # Increment the attempt counter
+    ((attempt++))
 
-# Loop until currUser is successfully retrieved
-while [[ -z "$currUser" ]]; do
+    # Initialize currUser as an empty string
+    currUser=""
+
     # Attempt to get the current user's name
     currUser=$(scutil <<< "show State:/Users/ConsoleUser" | awk '/Name :/ { print $3 }')
-    echo whoami=$currUser
-    # Check if currUser is still empty
-    if [[ -z "$currUser" ]]; then
-        echo "[ERROR] - Current user not found. Retrying..."
-        # Optionally, you can add a delay here before the next iteration
-        sleep 1
+
+    # Check if currUser is 'loginwindow' or empty
+    if [[ -z "$currUser" || "$currUser" == "loginwindow" ]]; then
+    echo "[ERROR] - Valid current user not found (user is '$currUser'). Retrying..."
+    sleep 1
+    continue
     fi
+
+    # If currUser is empty, retry the loop
+    #if [[ -z "$currUser" ]]; then
+    #    echo "[ERROR] - Current user not found. Retrying..."
+    #    sleep 1
+    #    continue
+    #fi
+
+    echo "whoami=$currUser"
+
+    # Get uid logged in user
+    uid=$(id -u "${currUser}")
+
+    # Define the runasuser function
+    runasuser() {
+        launchctl asuser "$uid" sudo -u "$currUser" "$@"
+    }
+
+    # Write the new setting
+    runasuser /usr/bin/defaults write com.apple.Home showHomeEnergy -bool false
+    # AND as root
+    /usr/bin/defaults write com.apple.Home showHomeEnergy -bool false
+    # Read the new status
+    new_shestatus=$(/usr/bin/defaults read com.apple.Home showHomeEnergy)
+
+        echo "Debug: new_shestatus is '$new_shestatus'"
+
+    #Compare the new status to the expected value
+    if [[ "$new_shestatus" == "0" ]]; then
+      echo "[SUCCESS] - Home Energy Location popup suppressed"
+      echo "[EXIT] - Exiting script."
+      sleep 5
+      pkill WindowManager
+      exit
+    else
+        echo "[FAIL] - Home Energy Location popup NOT suppressed"
+        echo "Attempt $attempt of $max_retries."
+       # add a sleep delay here if you want before the next iteration
+       sleep 1
+    fi
+
 done
 
-# Get uid logged in user
-uid=$(id -u "${currUser}")
-
-# Define the runasuser function
-runasuser() {
-    launchctl asuser "$uid" sudo -u "$currUser" "$@"
-}
-
-runasuser /usr/bin/defaults write com.apple.Home showHomeEnergy -bool false
-
-# Compare the new status to the expected value
-new_shestatus=$(launchctl asuser "$uid" sudo -u "$currUser" "$@" /usr/bin/defaults read com.apple.Home showHomeEnergy)
-
-echo "Debug: new_shestatus is '$new_shestatus'"
-if [[ "$new_shestatus" == "0" ]]; then
-    echo "[SUCCESS] - Home Energy Location popup suppressed"
-    echo "[EXIT] - Exiting script."
-    pkill WindowManager
-    sleep 1
-    exit 0
-else
-    echo "[FAIL] - Home Energy Location popup NOT suppressed"
-    echo "Exiting script with error code 1"
-    exit 1
-fi
+echo "Exiting script after $max_retries attempts with error code 1"
+exit 1
