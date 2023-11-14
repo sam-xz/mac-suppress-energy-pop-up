1b550f82afb7d5a06574b2f44c7c9b5bd613a185
Samuel Marino <samuel.marino@admins-MacBook-Pro.local>
2023-11-14 10:24:58 +0000

    changes

--- mac-suppress-energy-pop-up.sh
+++ mac-suppress-energy-pop-up.sh
@@ -1,7 +1,7 @@
 #!/bin/zsh
 
 # This script has a WTFPL License. For the greater good.
-# Supress the home energy pop up setting up fresh Sonoma macs. Example https://imgur.com/a/ZcMruTp
+# Suppress the home energy pop up setting up fresh Sonoma macs. Example https://imgur.com/a/ZcMruTp
 # You need to run this script as root via your MDM of choice. 
 # Samuel Marino 13.Nov.23
 
@@ -10,20 +10,10 @@ currUser=$( echo "show State:/Users/ConsoleUser" | scutil | awk '/Name :/ { prin
 uid=$(id -u "${currUser}")
 shestatus=$(/usr/bin/defaults read com.apple.Home showHomeEnergy)
 
-runAsUser() {  
-	if [[ "${currentUser}" != "loginwindow" ]]; then
-		launchctl asuser "$uid" sudo -u "${currentUser}" "$@"
-	else
-		echo "[FAIL] No user logged in."
-        echo "Exiting script with error code 1"
-		exit 1
-	fi
-}
-
-runAsUser /usr/bin/defaults write com.apple.Home showHomeEnergy -bool false
+launchctl asuser $uid sudo -u ${currentUser} "$@" /usr/bin/defaults write com.apple.Home showHomeEnergy -bool false
 sleep 3
 
-if [[ $shestatus == "0" ]]; then
+if [[ launchctl asuser "$uid" sudo -u "${currentUser}" "$@" $shestatus == "0" ]]; then
     echo "[SUCCESS] - Home Energy Location popup suppressed"
     echo "[EXIT] - Exiting script."
     exit 0
